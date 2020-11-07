c2d49ad605a0ae2de5f69ce164c3a8c77ba79c7b
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-11-07 15:36:37 +0300

    Adding fragment with java functionality is added.

--- app/src/main/java/com/alperenozil/fragmentexercise/MainActivity.java
+++ app/src/main/java/com/alperenozil/fragmentexercise/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BlankFragment blankFragment=new BlankFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, blankFragment)
+                .commit();
     }
 }

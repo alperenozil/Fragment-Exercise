737ad763a7589b86618e7b7ec94b4e80b440045a
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-11-07 18:04:17 +0300

    Fragment adding and removing work well.

--- app/src/main/java/com/alperenozil/fragmentexercise/MainActivity.java
+++ app/src/main/java/com/alperenozil/fragmentexercise/MainActivity.java
@@ -1,19 +1,34 @@
 package com.alperenozil.fragmentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BlankFragment blankFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void add(View view){
         BlankFragment blankFragment=new BlankFragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_container, blankFragment)
                 .commit();
+
+    }
+    public void remove(View view){
+        Fragment blankFragment=getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if(blankFragment!=null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(blankFragment)
+                    .commit();
+        }
     }
 }
